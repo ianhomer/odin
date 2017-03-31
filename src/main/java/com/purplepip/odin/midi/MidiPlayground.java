4049f3cab85b5bbbd2456829ff815868202b03e0
Ian Homer <ian.homer@gmail.com>
2017-03-31 14:58:58 +0100

    Use log API Extend MIDI playground

--- src/main/java/com/purplepip/odin/midi/MidiPlayground.java
+++ src/main/java/com/purplepip/odin/midi/MidiPlayground.java
@@ -1,41 +1,88 @@
 package com.purplepip.odin.midi;
 
-import javax.sound.midi.MidiSystem;
-import javax.sound.midi.MidiUnavailableException;
-import javax.sound.midi.Receiver;
-import javax.sound.midi.Transmitter;
+import com.purplepip.odin.server.Application;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+
+import javax.sound.midi.*;
 import java.util.Arrays;
 import java.util.stream.Collectors;
 
 /**
- * Midi server.
+ * MIDI playground.
  */
 public class MidiPlayground {
+    private static final Logger LOG = LoggerFactory.getLogger(Application.class);
+
     public static void main(String [] args) {
         MidiPlayground playground = new MidiPlayground();
         playground.dumpInfo();
     }
 
     private void dumpInfo() {
-        Arrays.stream(MidiSystem.getMidiDeviceInfo()).collect(Collectors.toList()).forEach((temp) -> {
-            System.out.println("Midi device info : " + temp.getDescription());
-        });
+        Arrays.stream(MidiSystem.getMidiDeviceInfo()).collect(Collectors.toList()).forEach((temp) ->
+                LOG.info("MIDI device info : " + temp.getDescription()));
 
-        Receiver receiver = null;
+        ShortMessage middleC = new ShortMessage();
+        ShortMessage middleD = new ShortMessage();
+        try {
+            middleC.setMessage(ShortMessage.NOTE_ON, 1, 60, 93);
+            middleD.setMessage(ShortMessage.NOTE_ON, 1, 62, 60);
+        } catch (InvalidMidiDataException e) {
+            LOG.error("Cannot create short message", e);
+            return;
+        }
+
+        Receiver receiver;
         try {
             receiver = MidiSystem.getReceiver();
+            //MidiSystem.getSynthesizer().getTransmitter().setReceiver(receiver);
+            LOG.info("Receiver : " + receiver);
+            receiver.send(middleC, 100);
+            LOG.info("Sent note");
         } catch (MidiUnavailableException e) {
-            e.printStackTrace();
+            LOG.error("Cannot get MIDI receiver", e);
         }
-        System.out.println("Receiver : " + receiver);
 
-        Transmitter transmitter = null;
+        Transmitter transmitter;
         try {
             transmitter = MidiSystem.getTransmitter();
+            LOG.info("Transmitter : " + transmitter);
         } catch (MidiUnavailableException e) {
-            e.printStackTrace();
+            LOG.error("Cannot get MIDI transmitter", e);
         }
 
-        System.out.println("Transmitter : " + transmitter);
+        Sequencer sequencer;
+        try {
+            sequencer = MidiSystem.getSequencer();
+            sequencer.open();
+            LOG.info("Sequencer : " + sequencer + " ; " + getDetails(sequencer));
+        } catch (MidiUnavailableException e) {
+            LOG.error("Cannot get MIDI sequencer", e);
+        }
+
+        Synthesizer synthesizer;
+        try {
+            synthesizer = MidiSystem.getSynthesizer();
+            LOG.info("Synthesizer : " + synthesizer + " ; " + getDetails(synthesizer));
+            synthesizer.open();
+            synthesizer.getReceiver().send(middleC, -1);
+            synthesizer.getReceiver().send(middleD, -1);
+            LOG.info("Sent notes to synthesizer");
+        } catch (MidiUnavailableException e) {
+            LOG.error("Cannot get MIDI synthesizer", e);
+        }
+
+        try {
+            Thread.sleep(5000);
+        } catch (InterruptedException e) {
+            LOG.error("Sleep interrupted", e);
+        }
+        LOG.info("... stopping");
+    }
+
+    private String getDetails(MidiDevice midiDevice) {
+        return "number of transmitters = " + midiDevice.getTransmitters().size()
+                + "; number of receivers = " + midiDevice.getReceivers().size();
     }
 }
--- src/main/java/com/purplepip/odin/server/Application.java
+++ src/main/java/com/purplepip/odin/server/Application.java
@@ -1,5 +1,7 @@
 package com.purplepip.odin.server;
 
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
 import org.springframework.boot.CommandLineRunner;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
@@ -11,6 +13,8 @@ import org.springframework.context.annotation.Bean;
  */
 @SpringBootApplication
 public class Application {
+    private static final Logger LOG = LoggerFactory.getLogger(Application.class);
+
     public static void main(String[] args) {
         SpringApplication.run(Application.class, args);
     }
@@ -18,7 +22,7 @@ public class Application {
     @Bean
     public CommandLineRunner commandLineRunner(ApplicationContext ctx) {
         return args -> {
-            System.out.println("Odin Started.");
+            LOG.info("Odin Started.");
         };
     }
 }
