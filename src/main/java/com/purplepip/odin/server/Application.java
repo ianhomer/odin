be13b5c7a0b73a711d0dad9ac6165ab72cc1de1c
Ian Homer <ian.homer@gmail.com>
2017-03-31 12:20:07 +0100

    Create Spring Boot Structure

---
+++ src/main/java/com/purplepip/odin/server/Application.java
@@ -0,0 +1,25 @@
+package com.purplepip.odin.server;
+
+import org.springframework.boot.CommandLineRunner;
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.context.ApplicationContext;
+import org.springframework.context.annotation.Bean;
+
+/**
+ * Odin Application.
+ */
+@SpringBootApplication
+public class Application {
+    public static void main(String[] args) {
+        SpringApplication.run(Application.class, args);
+    }
+
+    @Bean
+    public CommandLineRunner commandLineRunner(ApplicationContext ctx) {
+        return args -> {
+            System.out.println("Odin Started.");
+        };
+    }
+}
+
---
+++ src/main/java/com/purplepip/odin/server/MainController.java
@@ -0,0 +1,15 @@
+package com.purplepip.odin.server;
+
+import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.bind.annotation.RequestMapping;
+
+/**
+ * Odin Main Controller.
+ */
+@RestController
+public class MainController {
+    @RequestMapping("/")
+    public String index() {
+        return "Hello from Odin.";
+    }
+}
