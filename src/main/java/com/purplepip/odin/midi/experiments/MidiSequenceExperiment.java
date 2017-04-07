6cc2f25e2f9960704f87d3f238a787466bebc0e7
Ian Homer <ian.homer@gmail.com>
2017-04-07 12:05:01 +0100

    Create a SequenceFactory to generate a Sequence from a Metronome series

---
+++ src/main/java/com/purplepip/odin/midi/experiments/MidiSequenceExperiment.java
@@ -0,0 +1,21 @@
+package com.purplepip.odin.midi.experiments;
+
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+
+/**
+ * Midi Sequence Experiment.
+ */
+public class MidiSequenceExperiment {
+    private static final Logger LOG = LoggerFactory.getLogger(MidiSequenceExperiment.class);
+
+    public static void main(String [] args) {
+        MidiSequenceExperiment experiment = new MidiSequenceExperiment();
+        experiment.doExperiment();
+    }
+
+    private void doExperiment() {
+        
+    }
+
+}
---
+++ src/test/java/com/purplepip/odin/midi/SequenceFactoryTest.java
@@ -0,0 +1,8 @@
+import static org.junit.jupiter.api.Assertions.*;
+
+/**
+ * Created by ian on 07/04/2017.
+ */
+class SequenceFactoryTest {
+
+}
