acbe46c735c1c43af9c2b64e5c3bfae2613fae65
Ian Homer <ian.homer@gmail.com>
2017-04-15 16:36:27 +0100

    Add the default tick converter

--- src/main/java/com/purplepip/odin/series/DefaultTickConverter.java
+++ src/main/java/com/purplepip/odin/series/DefaultTickConverter.java
@@ -6,68 +6,52 @@ import org.slf4j.LoggerFactory;
 /**
  * Time unit converter.
  */
-public class TickConverter {
-    private static final Logger LOG = LoggerFactory.getLogger(TickConverter.class);
+public class DefaultTickConverter extends AbstractTickConverter {
+    private static final Logger LOG = LoggerFactory.getLogger(DefaultTickConverter.class);
 
-    private long inputOffset;
     private Clock clock;
-    private Tick inputTick;
-    private Tick outputTick;
 
-    public TickConverter(Clock clock) {
+    public DefaultTickConverter(Clock clock) {
         this(clock, Tick.BEAT);
     }
 
-    public TickConverter(Clock clock, Tick inputTick) {
+    public DefaultTickConverter(Clock clock, Tick inputTick) {
         this(clock, inputTick, Tick.MICROSECOND);
     }
 
-    public TickConverter(Clock clock, Tick inputTick, Tick outputTick) {
+    public DefaultTickConverter(Clock clock, Tick inputTick, Tick outputTick) {
         this(clock, inputTick, outputTick, 0);
     }
 
-    public TickConverter(Clock clock, Tick inputTick, Tick outputTick, long inputOffset) {
+    public DefaultTickConverter(Clock clock, Tick inputTick, Tick outputTick, long inputOffset) {
         if (clock == null) {
             throw new RuntimeException("Clock must not be null");
         }
         this.clock = clock;
-        this.inputTick = inputTick;
-        this.outputTick = outputTick;
-        this.inputOffset = inputOffset;
+        setInputTick(inputTick);
+        setOutputTick(outputTick);
+        setInputOffset(inputOffset);
     }
 
-    public Tick getOutputTick() {
-        return outputTick;
-    }
-
-    public long convert(long time) {
-        LOG.trace("Converting {} from {} to {}", time, inputTick, outputTick);
-        return convertTimeUnit(inputOffset + time);
-    }
-
-    private long scaleTime(long time) {
-        return time * inputTick.getNumerator() * outputTick.getDenominator() /
-                (inputTick.getDenominator() * outputTick.getNumerator());
-    }
-
-    private long convertTimeUnit(long time) {
-        switch (inputTick.getTimeUnit()) {
+    @Override
+    protected long convertTimeUnit(long time) {
+        switch (getInputTick().getTimeUnit()) {
             case BEAT:
-                switch (outputTick.getTimeUnit()) {
+                switch (getOutputTick().getTimeUnit()) {
                     case BEAT:
                         return scaleTime(time);
                     case MICROSECOND:
-                        return clock.getMicroSeconds(inputTick.getFactor() * time) / outputTick.getFactorAsInt();
+                        return clock.getMicroSeconds(getInputTick().getFactor() * time) / getOutputTick().getFactorAsInt();
                 }
             case MICROSECOND:
-                switch (outputTick.getTimeUnit()) {
+                switch (getOutputTick().getTimeUnit()) {
                     case BEAT:
-                        return (long) (clock.getBeat(inputTick.getFactorAsInt() * time) / outputTick.getFactor());
+                        return (long) (clock.getBeat(getInputTick().getFactorAsInt() * time) / getOutputTick().getFactor());
                     case MICROSECOND:
                         return scaleTime(time);
                 }
         }
-        throw new RuntimeException("Unexpected time unit " + inputTick.getTimeUnit() + ":" +
-            outputTick.getTimeUnit());
+        throw new RuntimeException("Unexpected time unit " + getInputTick().getTimeUnit() + ":" +
+            getOutputTick().getTimeUnit());
     }
 }
