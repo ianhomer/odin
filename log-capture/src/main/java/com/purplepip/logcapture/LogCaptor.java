98f2a02b02b2c7a51979f4b5473c11aed25a2559
Ian Homer <ian.homer@gmail.com>
2017-06-06 09:55:27 +0100

    Test coverage for Log Catpure

--- log-capture/src/main/java/com/purplepip/logcapture/LogCaptor.java
+++ log-capture/src/main/java/com/purplepip/logcapture/LogCaptor.java
@@ -1,57 +1,77 @@
 package com.purplepip.logcapture;
 
+import ch.qos.logback.classic.Level;
 import ch.qos.logback.classic.Logger;
 import ch.qos.logback.classic.LoggerContext;
 import ch.qos.logback.classic.spi.ILoggingEvent;
 import ch.qos.logback.core.Appender;
 import ch.qos.logback.core.read.ListAppender;
 import com.google.common.collect.ImmutableList;
-
-import java.util.ArrayList;
-import java.util.List;
-
+import java.util.HashMap;
+import java.util.Map;
 import org.slf4j.LoggerFactory;
 
 /**
  * Log capture tool.
  */
-public class LogCapture implements AutoCloseable {
+public class LogCaptor implements AutoCloseable {
   private LogCaptureConfiguration configuration;
+  private Level originalLevel;
 
-  LogCapture(LogCaptureConfiguration configuration) {
+  LogCaptor(LogCaptureConfiguration configuration) {
     this.configuration = configuration;
-    open();
+    start();
   }
 
   private ListAppender<ILoggingEvent> capturingAppender = new ListAppender<>();
-  private LoggerContext loggerContext = (LoggerContext) LoggerFactory.getILoggerFactory();
+  private LoggerContext context = (LoggerContext) LoggerFactory.getILoggerFactory();
 
-  private List<Appender<ILoggingEvent>> removedAppenders = new ArrayList<>();
+  private Map<String, Appender<ILoggingEvent>> removedAppenders = new HashMap<>();
 
-  private void open() {
-    Logger logger = loggerContext.getLogger(configuration.getCategory());
-
-    for (Appender<ILoggingEvent> appenderToRemove :
-        ImmutableList.copyOf(logger.iteratorForAppenders())) {
-      logger.detachAppender(appenderToRemove);
-      removedAppenders.add(appenderToRemove);
+  private void start() {
+    /*
+     * Remove all appenders.
+     */
+    for (Logger logger : context.getLoggerList()) {
+      for (Appender<ILoggingEvent> appender :
+          ImmutableList.copyOf(logger.iteratorForAppenders())) {
+        logger.detachAppender(appender);
+        removedAppenders.put(logger.getName(), appender);
+      }
     }
 
-    capturingAppender.setContext(loggerContext);
+    /*
+     * Set up the capturing appender.
+     */
+    Logger logger = context.getLogger(configuration.getCategory());
+    capturingAppender.setContext(context);
     logger.addAppender(capturingAppender);
+    originalLevel = logger.getLevel();
+    logger.setLevel(configuration.getLevel());
     capturingAppender.start();
   }
 
   @Override
   public void close() {
-    Logger logger = loggerContext.getLogger(configuration.getCategory());
+    detachCapturingAppender();
 
+    /*
+     * Restore previous appenders.
+     */
+    for (Map.Entry<String, Appender<ILoggingEvent>> entry : removedAppenders.entrySet()) {
+      Logger logger = context.getLogger(entry.getKey());
+      logger.addAppender(entry.getValue());
+    }
+  }
+
+  /*
+   * Remove capturing appender.
+   */
+  private void detachCapturingAppender() {
+    Logger logger = context.getLogger(configuration.getCategory());
     capturingAppender.stop();
     logger.detachAppender(capturingAppender);
-
-    for (Appender<ILoggingEvent> appender : removedAppenders) {
-      logger.addAppender(appender);
-    }
+    logger.setLevel(originalLevel);
   }
 
   public int size() {
