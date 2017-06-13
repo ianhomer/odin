1c5d5b2b6f2aed012db5f0793165c3c82f2b58bf
Ian Homer <ian.homer@gmail.com>
2017-06-13 10:00:09 +0100

    Channels now displayed through front end

--- odin-server/src/main/js/app.js
+++ odin-server/src/main/js/app.js
@@ -12,18 +12,24 @@ class App extends React.Component {
 
 	constructor(props) {
 		super(props);
-		this.state = {patterns: []};
+		this.state = {patterns: [], channels: []};
 	}
 
 	componentDidMount() {
 		client({method: 'GET', path: '/api/patterns'}).done(response => {
 			this.setState({patterns: response.entity._embedded.patterns});
 		});
+		client({method: 'GET', path: '/api/channels'}).done(response => {
+			this.setState({channels: response.entity._embedded.channels});
+		});
 	}
 
 	render() {
 		return (
-			<PatternList patterns={this.state.patterns}/>
+		  <div>
+        <PatternList patterns={this.state.patterns}/>
+        <ChannelList channels={this.state.channels}/>
+			</div>
 		)
 	}
 }
@@ -62,6 +68,40 @@ class Pattern extends React.Component{
 	}
 }
 // end::pattern[]
+
+// tag::channel-list[]
+class ChannelList extends React.Component{
+	render() {
+		var channels = this.props.channels.map(channel =>
+			<Channel key={channel._links.self.href} channel={channel}/>
+		);
+		return (
+			<table>
+				<tbody>
+					<tr>
+						<th>Number</th>
+						<th>Program</th>
+					</tr>
+					{channels}
+				</tbody>
+			</table>
+		)
+	}
+}
+// end::channel-list[]
+
+// tag::channel[]
+class Channel extends React.Component{
+	render() {
+		return (
+			<tr>
+				<td>{this.props.channel.number}</td>
+				<td>{this.props.channel.programName}</td>
+			</tr>
+		)
+	}
+}
+// end::channel[]
 
 // tag::render[]
 ReactDOM.render(
