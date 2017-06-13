5c1b18b2b75f3342830f325d3d42260873eca857
Ian Homer <ian.homer@gmail.com>
2017-06-13 09:36:23 +0100

    Use Thymeleaf layout decorator Split system pages into multiple pages

--- odin-server/src/main/java/com/purplepip/odin/server/MainController.java
+++ odin-server/src/main/java/com/purplepip/odin/server/MainController.java
@@ -15,11 +15,6 @@
 
 package com.purplepip.odin.server;
 
-import com.purplepip.odin.midix.MidiDeviceWrapper;
-import com.purplepip.odin.midix.MidiSystemWrapper;
-import com.purplepip.odin.sequence.measure.MeasureProvider;
-import com.purplepip.odin.sequencer.OdinSequencer;
-import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.RequestMapping;
@@ -29,18 +24,6 @@ import org.springframework.web.bind.annotation.RequestMapping;
  */
 @Controller
 public class MainController {
-  @Autowired
-  private MidiDeviceWrapper midiDeviceWrapper;
-
-  @Autowired
-  private MeasureProvider measureProvider;
-
-  @Autowired
-  private OdinSequencer sequencer;
-
-  @Autowired
-  private MidiSystemWrapper midiSystemWrapper;
-
   /**
    * Index page.
    *
@@ -50,20 +33,5 @@ public class MainController {
   @RequestMapping("/")
   public String index(Model model) {
     return "index";
-  }
-
-  /**
-   * System page.
-   *
-   * @param model model
-   * @return template to render
-   */
-  @RequestMapping("/system")
-  public String system(Model model) {
-    model.addAttribute("midiSystem", midiSystemWrapper);
-    model.addAttribute("midiDevice", midiDeviceWrapper);
-    model.addAttribute("measureProvider", measureProvider);
-    model.addAttribute("sequencer", sequencer);
-    return "system";
   }
 }
