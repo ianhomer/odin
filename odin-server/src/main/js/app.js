a5033a54774db963277b8327a8577ce5893791a2
Ian Homer <ian.homer@gmail.com>
2017-06-13 12:27:20 +0100

    Move channel list into separate file

--- odin-server/src/main/js/app.js
+++ odin-server/src/main/js/app.js
@@ -6,8 +6,9 @@ const ReactDOM = require('react-dom');
 const client = require('./client');
 const follow = require('./follow');
 const root = '/api';
-const PatternList = require('./patternList')
-const ProjectList = require('./projectList')
+const ChannelList = require('./model/channelList')
+const PatternList = require('./model/patternList')
+const ProjectList = require('./model/projectList')
 // end::vars[]
 
 
@@ -113,14 +114,13 @@ class App extends React.Component {
 		  <div>
         <h1>Projects</h1>
         <ProjectList projects={this.state.projects}/>
-		    <h1>Channels</h1>
-		    <CreateDialog attributes={this.state.attributes} onCreate={this.onCreate}/>
         <ChannelList channels={this.state.channels}
                       links={this.state.links}
           						pageSize={this.state.pageSize}
           						onNavigate={this.onNavigate}
           						onDelete={this.onDelete}
-          						updatePageSize={this.updatePageSize}/>
+          						updatePageSize={this.updatePageSize}
+          						attributes={this.state.attributes}/>
         <h1>Patterns</h1>
         <PatternList patterns={this.state.patterns}/>
 			</div>
@@ -128,173 +128,6 @@ class App extends React.Component {
 	}
 }
 // end::app[]
-
-// tag::create-dialog[]
-class CreateDialog extends React.Component {
-
-	constructor(props) {
-		super(props);
-		this.handleSubmit = this.handleSubmit.bind(this);
-	}
-
-	handleSubmit(e) {
-		e.preventDefault();
-		var newChannel = {};
-		this.props.attributes.forEach(attribute => {
-			newChannel[attribute] = ReactDOM.findDOMNode(this.refs[attribute]).value.trim();
-		});
-		this.props.onCreate(newChannel);
-
-		// clear out the dialog's inputs
-		this.props.attributes.forEach(attribute => {
-			ReactDOM.findDOMNode(this.refs[attribute]).value = '';
-		});
-
-		// Navigate away from the dialog to hide it.
-		window.location = "#";
-	}
-
-	render() {
-		var inputs = this.props.attributes.map(attribute =>
-			<p key={attribute}>
-				<input type="text" placeholder={attribute} ref={attribute} className="field" />
-			</p>
-		);
-
-		return (
-			<div>
-				<a href="#createChannel">Create</a>
-
-				<div id="createChannel" className="modalDialog">
-					<div>
-						<a href="#" title="Close" className="close">X</a>
-
-						<h2>Create new channel</h2>
-
-						<form>
-							{inputs}
-							<button onClick={this.handleSubmit}>Create</button>
-						</form>
-					</div>
-				</div>
-			</div>
-		)
-	}
-
-}
-// end::create-dialog[]
-
-// tag::channel-list[]
-class ChannelList extends React.Component{
-	constructor(props) {
-		super(props);
-		this.handleNavFirst = this.handleNavFirst.bind(this);
-		this.handleNavPrev = this.handleNavPrev.bind(this);
-		this.handleNavNext = this.handleNavNext.bind(this);
-		this.handleNavLast = this.handleNavLast.bind(this);
-		this.handleInput = this.handleInput.bind(this);
-	}
-
-	// tag::handle-page-size-updates[]
-  handleInput(e) {
-  		e.preventDefault();
-  		var pageSize = ReactDOM.findDOMNode(this.refs.pageSize).value;
-  		if (/^[0-9]+$/.test(pageSize)) {
-  			this.props.updatePageSize(pageSize);
-  		} else {
-  			ReactDOM.findDOMNode(this.refs.pageSize).value =
-  				pageSize.substring(0, pageSize.length - 1);
-  		}
-  	}
-  	// end::handle-page-size-updates[]
-
-  	// tag::handle-nav[]
-  	handleNavFirst(e){
-  		e.preventDefault();
-  		this.props.onNavigate(this.props.links.first.href);
-  	}
-
-  	handleNavPrev(e) {
-  		e.preventDefault();
-  		this.props.onNavigate(this.props.links.prev.href);
-  	}
-
-  	handleNavNext(e) {
-  		e.preventDefault();
-  		this.props.onNavigate(this.props.links.next.href);
-  	}
-
-  	handleNavLast(e) {
-  		e.preventDefault();
-  		this.props.onNavigate(this.props.links.last.href);
-  	}
-  	// end::handle-nav[]
-
-  render() {
-      var channels = this.props.channels.map(channel =>
-        <Channel key={channel._links.self.href} channel={channel} onDelete={this.props.onDelete}/>
-      );
-
-      var navLinks = [];
-      if ("first" in this.props.links) {
-        navLinks.push(<button key="first" onClick={this.handleNavFirst}>&lt;&lt;</button>);
-      }
-      if ("prev" in this.props.links) {
-        navLinks.push(<button key="prev" onClick={this.handleNavPrev}>&lt;</button>);
-      }
-      if ("next" in this.props.links) {
-        navLinks.push(<button key="next" onClick={this.handleNavNext}>&gt;</button>);
-      }
-      if ("last" in this.props.links) {
-        navLinks.push(<button key="last" onClick={this.handleNavLast}>&gt;&gt;</button>);
-      }
-
-		return (
-
-      <div>
-        <input ref="pageSize" defaultValue={this.props.pageSize} onInput={this.handleInput}/>
-        <table>
-          <tbody>
-            <tr>
-              <th>Number</th>
-              <th>Program</th>
-            </tr>
-            {channels}
-          </tbody>
-        </table>
-				<div>
-					{navLinks}
-				</div>
-			</div>
-		)
-	}
-}
-// end::channel-list[]
-
-// tag::channel[]
-class Channel extends React.Component{
-	constructor(props) {
-		super(props);
-		this.handleDelete = this.handleDelete.bind(this);
-	}
-
-	handleDelete() {
-		this.props.onDelete(this.props.channel);
-	}
-
-	render() {
-			return (
-			<tr>
-				<td>{this.props.channel.number}</td>
-				<td>{this.props.channel.programName}</td>
-				<td>
-					<button onClick={this.handleDelete}>Delete</button>
-				</td>
-			</tr>
-		)
-	}
-}
-// end::channel[]
 
 // tag::render[]
 ReactDOM.render(
---
+++ odin-server/src/main/js/model/channelList.js
@@ -0,0 +1,171 @@
+var React = require('react');
+
+// tag::create-dialog[]
+class CreateDialog extends React.Component {
+
+	constructor(props) {
+		super(props);
+		this.handleSubmit = this.handleSubmit.bind(this);
+	}
+
+	handleSubmit(e) {
+		e.preventDefault();
+		var newChannel = {};
+		this.props.attributes.forEach(attribute => {
+			newChannel[attribute] = ReactDOM.findDOMNode(this.refs[attribute]).value.trim();
+		});
+		this.props.onCreate(newChannel);
+
+		// clear out the dialog's inputs
+		this.props.attributes.forEach(attribute => {
+			ReactDOM.findDOMNode(this.refs[attribute]).value = '';
+		});
+
+		// Navigate away from the dialog to hide it.
+		window.location = "#";
+	}
+
+	render() {
+		var inputs = this.props.attributes.map(attribute =>
+			<p key={attribute}>
+				<input type="text" placeholder={attribute} ref={attribute} className="field" />
+			</p>
+		);
+
+		return (
+			<div>
+				<a href="#createChannel">Create</a>
+
+				<div id="createChannel" className="modalDialog">
+					<div>
+						<a href="#" title="Close" className="close">X</a>
+
+						<h2>Create new channel</h2>
+
+						<form>
+							{inputs}
+							<button onClick={this.handleSubmit}>Create</button>
+						</form>
+					</div>
+				</div>
+			</div>
+		)
+	}
+
+}
+// end::create-dialog[]
+
+// tag::channel-list[]
+class ChannelList extends React.Component{
+	constructor(props) {
+		super(props);
+		this.handleNavFirst = this.handleNavFirst.bind(this);
+		this.handleNavPrev = this.handleNavPrev.bind(this);
+		this.handleNavNext = this.handleNavNext.bind(this);
+		this.handleNavLast = this.handleNavLast.bind(this);
+		this.handleInput = this.handleInput.bind(this);
+	}
+
+	// tag::handle-page-size-updates[]
+  handleInput(e) {
+    e.preventDefault();
+    var pageSize = ReactDOM.findDOMNode(this.refs.pageSize).value;
+    if (/^[0-9]+$/.test(pageSize)) {
+      this.props.updatePageSize(pageSize);
+    } else {
+      ReactDOM.findDOMNode(this.refs.pageSize).value =
+        pageSize.substring(0, pageSize.length - 1);
+    }
+  }
+  // end::handle-page-size-updates[]
+
+  // tag::handle-nav[]
+  handleNavFirst(e){
+    e.preventDefault();
+    this.props.onNavigate(this.props.links.first.href);
+  }
+
+  handleNavPrev(e) {
+    e.preventDefault();
+    this.props.onNavigate(this.props.links.prev.href);
+  }
+
+  handleNavNext(e) {
+    e.preventDefault();
+    this.props.onNavigate(this.props.links.next.href);
+  }
+
+  handleNavLast(e) {
+    e.preventDefault();
+    this.props.onNavigate(this.props.links.last.href);
+  }
+  // end::handle-nav[]
+
+  render() {
+    var channels = this.props.channels.map(channel =>
+      <Channel key={channel._links.self.href} channel={channel} onDelete={this.props.onDelete}/>
+    );
+
+    var navLinks = [];
+    if ("first" in this.props.links) {
+      navLinks.push(<button key="first" onClick={this.handleNavFirst}>&lt;&lt;</button>);
+    }
+    if ("prev" in this.props.links) {
+      navLinks.push(<button key="prev" onClick={this.handleNavPrev}>&lt;</button>);
+    }
+    if ("next" in this.props.links) {
+      navLinks.push(<button key="next" onClick={this.handleNavNext}>&gt;</button>);
+    }
+    if ("last" in this.props.links) {
+      navLinks.push(<button key="last" onClick={this.handleNavLast}>&gt;&gt;</button>);
+    }
+
+		return (
+      <div>
+        <h1>Channels</h1>
+        <CreateDialog attributes={this.props.attributes} onCreate={this.onCreate}/>
+        <input ref="pageSize" defaultValue={this.props.pageSize} onInput={this.handleInput}/>
+        <table>
+          <tbody>
+            <tr>
+              <th>Number</th>
+              <th>Program</th>
+            </tr>
+            {channels}
+          </tbody>
+        </table>
+				<div>
+					{navLinks}
+				</div>
+			</div>
+		)
+	}
+}
+// end::channel-list[]
+
+// tag::channel[]
+class Channel extends React.Component{
+	constructor(props) {
+		super(props);
+		this.handleDelete = this.handleDelete.bind(this);
+	}
+
+	handleDelete() {
+		this.props.onDelete(this.props.channel);
+	}
+
+	render() {
+			return (
+			<tr>
+				<td>{this.props.channel.number}</td>
+				<td>{this.props.channel.programName}</td>
+				<td>
+					<button onClick={this.handleDelete}>Delete</button>
+				</td>
+			</tr>
+		)
+	}
+}
+// end::channel[]
+
+module.exports = ChannelList
