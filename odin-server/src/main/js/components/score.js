b1421695aee0d4d25b2b5d51f4afaaf74fa47cb7
Ian Homer <ian.homer@gmail.com>
2017-07-23 09:52:29 +0100

    Use easy score API instead

--- odin-server/src/main/js/components/score.js
+++ odin-server/src/main/js/components/score.js
@@ -28,25 +28,21 @@ class Score extends React.Component{
   }
 
   renderNotation() {
-    var VF = Vex.Flow;
+    var vf = new Vex.Flow.Factory({
+      renderer: {selector: 'boo', width: 500, height: 200}
+    });
 
-    // Create an SVG renderer and attach it to the DIV element named "boo".
-    var div = document.getElementById('boo');
-    var renderer = new VF.Renderer(div, VF.Renderer.Backends.SVG);
+    var score = vf.EasyScore();
+    var system = vf.System();
 
-    // Configure the rendering context.
-    renderer.resize(500, 200);
-    var context = renderer.getContext();
-    context.setFont('Arial', 10, '').setBackgroundFillStyle('#eed');
+    system.addStave({
+      voices: [
+        score.voice(score.notes('C#5/q, B4, A4, G#4', {stem: 'up'})),
+        score.voice(score.notes('C#4/h, C#4', {stem: 'down'}))
+      ]
+    }).addClef('treble').addTimeSignature('4/4');
 
-    // Create a stave of width 400 at position 10, 40 on the canvas.
-    var stave = new VF.Stave(10, 40, 400);
-
-    // Add a clef and time signature.
-    stave.addClef('treble').addTimeSignature('4/4');
-
-    // Connect it to the rendering context and draw!
-    stave.setContext(context).draw();
+    vf.draw();
   }
 
   render() {
