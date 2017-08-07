f33274eabfe58efc04f0ad9c20744248f6e88d03
Ian Homer <ian.homer@gmail.com>
2017-08-07 21:00:18 +0100

    Add test case for static beats per minute

---
+++ odin-core/src/test/java/com/purplepip/odin/sequence/StaticBeatsPerMinuteTest.java
@@ -0,0 +1,31 @@
+package com.purplepip.odin.sequence;
+
+import static org.junit.Assert.assertEquals;
+import static org.junit.Assert.assertTrue;
+
+import com.purplepip.odin.math.Real;
+import com.purplepip.odin.math.Whole;
+import org.junit.Test;
+
+/*
+ * Copyright (c) 2017 Ian Homer. All Rights Reserved
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *  http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+public class StaticBeatsPerMinuteTest {
+  @Test
+  public void testBeatsPerMinute() {
+    BeatsPerMinute bpm = new StaticBeatsPerMinute(60);
+    assertTrue(bpm.getMicroSecondsPerBeat() instanceof Whole);
+    assertEquals(Real.valueOf(1000000), bpm.getMicroSecondsPerBeat());
+  }
+}
