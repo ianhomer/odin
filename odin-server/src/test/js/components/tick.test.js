5b965bd6755501a43569411da03ef08ef4d45400
Ian Homer <ian.homer@gmail.com>
2017-09-08 15:40:13 +0100

    Unit test for Tick front end component

--- odin-server/src/test/js/components/tick.test.js
+++ odin-server/src/test/js/components/tick.test.js
@@ -1,10 +1,10 @@
 import React from 'react';
-import Link from '../Link.react';
+import Tick from 'components/tick.js';
 import renderer from 'react-test-renderer';
 
 test('Tick renders OK', () => {
   const component = renderer.create(
-    <Tick numerator='1' denominator='2'/>
+    <Tick numerator="1" denominator="2"/>
   );
   let tree = component.toJSON();
   expect(tree).toMatchSnapshot();
