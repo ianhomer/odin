0b92a1cf9909f7aabd3e44418e26f72d355eda00
Ian Homer <ian.homer@gmail.com>
2017-09-11 06:57:32 +0100

    Comment out notation test until we've decoupled notation from crud

--- odin-server/src/test/js/components/notation.test.js
+++ odin-server/src/test/js/components/notation.test.js
@@ -1,11 +1,33 @@
 import React from 'react';
-import Tick from 'components/tick.js';
+//import Notation from 'components/notation.js';
 import renderer from 'react-test-renderer';
 
-test('Tick renders OK', () => {
-  const component = renderer.create(
-    <Tick numerator="1" denominator="2"/>
-  );
-  let tree = component.toJSON();
-  expect(tree).toMatchSnapshot();
+test('Notation renders OK', () => {
+  var entity = {
+    'name' : 'test-name',
+    'flowName' : 'notation',
+    'channel' : 1,
+    'offset' : 0,
+    'length' : -1,
+    'tick' : {
+      'timeUnit' : 'BEAT',
+      'numerator' : 1,
+      'denominator' : 1
+    },
+    'properties' : {
+      'notation' : 'A/q G/8 A/q E',
+      'format' : 'natural'
+    },
+    'layers' : [
+      'a', 'c'
+    ]
+  };
+// Re-enable test when Notation decoupled from crud
+//  const component = renderer.create(
+//    <Notation entity={entity} key='key'
+//      schema={this.props.schema} project={this.props.project}
+//    />
+//  );
+//  let tree = component.toJSON();
+//  expect(tree).toMatchSnapshot();
 });
