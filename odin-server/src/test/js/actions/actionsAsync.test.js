3481cba3ba972a84f0e9ac410f668933bfdbe2ee
Ian Homer <ian.homer@gmail.com>
2017-09-20 23:21:12 +0100

    Reduce a little bit of redundant scaffolding around test dispatch and expect to make more readable

--- odin-server/src/test/js/actions/actionsAsync.test.js
+++ odin-server/src/test/js/actions/actionsAsync.test.js
@@ -9,39 +9,24 @@ describe('async actions', () => {
   })
 
   test('Create Channel 1', done => {
-    dispatchAndExpect(store, done, 'CREATE_ENTITY_SUCCEEDED', () => {
-      return createEntityRequested({
-        name : 'test-name-1',
-        number : 1
-      }, 'channel')
-    },
-    () => {
-      expect(store.getState().entities['channel'].entities.length).toBe(1)
-    })
+    dispatchAndExpect(store, done, 'CREATE_ENTITY_SUCCEEDED',
+      () => createEntityRequested({ name : 'test-name-1', number : 1}, 'channel'),
+      () => expect(store.getState().entities['channel'].entities.length).toBe(1)
+    )
   })
 
   test('Create Channel 2', done => {
-    dispatchAndExpect(store, done, 'CREATE_ENTITY_SUCCEEDED', () => {
-      return createEntityRequested({
-        name : 'test-name-2',
-        number : 2
-      }, 'channel')
-    },
-    () => {
-      expect(store.getState().entities['channel'].entities.length).toBe(2)
-    })
+    dispatchAndExpect(store, done, 'CREATE_ENTITY_SUCCEEDED',
+      () => createEntityRequested({ name : 'test-name-2', number : 2}, 'channel'),
+      () => expect(store.getState().entities['channel'].entities.length).toBe(2)
+    )
   })
 
   test('Delete Channel 1', done => {
-    dispatchAndExpect(store, done, 'DELETE_ENTITY_SUCCEEDED', () => {
-      return deleteEntityRequested({
-        name : 'test-name-1',
-        number : 1,
-        _links : { self : { href : 'http://localhost:8080/api/channel/7' }}
-      }, 'channel')
-    },
-    () => {
-      expect(store.getState().entities['channel'].entities.length).toBe(1)
-    })
+    dispatchAndExpect(store, done, 'DELETE_ENTITY_SUCCEEDED',
+      () => deleteEntityRequested({ name : 'test-name-1', number : 1,
+          _links : { self : { href : 'http://localhost:8080/api/channel/7' }}}, 'channel'),
+      () => expect(store.getState().entities['channel'].entities.length).toBe(1)
+    )
   })
 })
