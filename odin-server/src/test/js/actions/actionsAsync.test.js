4ab398d6e3ab75effa78d9784101571bfdc9840d
Ian Homer <ian.homer@gmail.com>
2017-09-20 23:08:17 +0100

    Format code to be consistent

--- odin-server/src/test/js/actions/actionsAsync.test.js
+++ odin-server/src/test/js/actions/actionsAsync.test.js
@@ -10,12 +10,10 @@ describe('async actions', () => {
 
   test('Create Channel 1', (done) => {
     dispatchAndExpect(store, done, 'CREATE_ENTITY_SUCCEEDED', () => {
-      return createEntityRequested(
-        {
-          name : 'test-name-1',
-          number : 1
-        }, 'channel'
-      )
+      return createEntityRequested({
+        name : 'test-name-1',
+        number : 1
+      }, 'channel')
     },
     () => {
       expect(store.getState().entities['channel'].entities.length).toBe(1)
