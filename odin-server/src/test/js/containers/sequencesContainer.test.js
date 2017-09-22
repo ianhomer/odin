0ba82a7bd85d7d0e2aad4aa5111f33e938a78911
Ian Homer <ian.homer@gmail.com>
2017-09-22 23:42:25 +0100

    Update dispatchAndExpect to dispatch multiple actions and wait for multiple actions to be fired

--- odin-server/src/test/js/containers/sequencesContainer.test.js
+++ odin-server/src/test/js/containers/sequencesContainer.test.js
@@ -1,23 +1,23 @@
 import React from 'react'
 import SequencesContainer from 'odin/containers/sequencesContainer.js'
-import { LOAD_PROFILE_SCHEMA_SUCCEEDED, loadSchemaActions } from 'odin/actions/index.js'
-import { Provider } from 'react-redux'
-import { mount } from 'enzyme'
+import { LOAD_PROJECT_SCHEMA_SUCCEEDED, LOAD_PROFILE_SCHEMA_SUCCEEDED,
+  loadSchemaActions } from 'odin/actions/index.js'
+import { shallow } from 'enzyme'
 
 import { dispatchAndExpect } from '../utils/dispatchAndExpect'
 import { mockFlux, testProject, testSchema } from '../testData.js'
 import store from '../store'
 
 test('Sequences container renders OK', done => {
-  const component = mount(
-    <Provider store={store}>
-      <SequencesContainer schema={testSchema} project={testProject} flux={mockFlux}/>
-    </Provider>
-  )
-  dispatchAndExpect(store, done, LOAD_PROFILE_SCHEMA_SUCCEEDED,
+  const component =
+    <SequencesContainer store={store} schema={testSchema} project={testProject} flux={mockFlux}/>
+  expect(shallow(component).dive()).toMatchSnapshot()
+
+  dispatchAndExpect(store, done,
+    [LOAD_PROJECT_SCHEMA_SUCCEEDED, LOAD_PROFILE_SCHEMA_SUCCEEDED, LOAD_PROFILE_SCHEMA_SUCCEEDED],
     () => loadSchemaActions,
     () => {
-      expect(component).toMatchSnapshot()
+      expect(shallow(component).dive()).toMatchSnapshot()
     }
   )
 })
--- odin-server/src/test/js/utils/dispatchAndExpect.js
+++ odin-server/src/test/js/utils/dispatchAndExpect.js
@@ -1,9 +1,12 @@
 export function dispatchAndExpect(store, done, waitForActionType, action, assertion) {
   var unsubscriber = {}
+  var waitForActionTypes = waitForActionType instanceof Array
+    ? [ ...waitForActionType ] : [ waitForActionType ]
   unsubscriber.unsubscribe = store.subscribe(() => {
     // The test redux store has a reducer that stores the last action so that we
     // can wait for the desired action type before executing the assertion
-    if (store.getState().lastAction.type === waitForActionType) {
+    waitForActionTypes = waitForActionTypes.filter(item => item !== store.getState().lastAction.type)
+    if (waitForActionTypes.length == 0) {
       try {
         assertion()
         unsubscriber.unsubscribe()
@@ -14,7 +17,7 @@ export function dispatchAndExpect(store, done, waitForActionType, action, assert
     }
   })
   var request = action()
-  if (request.constructor === Array) {
+  if (request instanceof Array) {
     for (var i = 0; i < request.length ; i++) {
       store.dispatch(request[i])
     }
