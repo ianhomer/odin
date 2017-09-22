41ffaa370bdb111b78f2bde501a86532f0d35a16
Ian Homer <ian.homer@gmail.com>
2017-09-22 17:21:59 +0100

    Use sinon spy to spy on calls to componentWillReceiveProps

--- odin-server/src/test/js/containers/app.test.js
+++ odin-server/src/test/js/containers/app.test.js
@@ -2,6 +2,7 @@
 
 import React from 'react'
 import { shallow } from 'enzyme'
+import sinon from 'sinon'
 
 import App from 'odin/containers/app.js'
 import { LOAD_PROJECT_SCHEMA_SUCCEEDED, loadProjectSchemaRequested } from 'odin/actions/index.js'
@@ -11,16 +12,25 @@ import store from '../store'
 import { dispatchAndExpect } from '../utils/dispatchAndExpect'
 
 describe('async actions', () => {
-  var app = <App store={store} flux={mockFlux} />
+  var props = {store, flux: mockFlux}
+  var app = <App {...props} />
 
   test('App before schema loaded OK', () => {
     expect(shallow(app).dive()).toMatchSnapshot()
   })
 
   test('App after schema loaded OK', done => {
+    var shallowApp = shallow(app)
+    const spyReceiveProps = sinon.spy(App.prototype, "componentWillReceiveProps");
     dispatchAndExpect(store, done, LOAD_PROJECT_SCHEMA_SUCCEEDED,
       () => loadProjectSchemaRequested(),
-      () => expect(shallow(app).dive()).toMatchSnapshot()
+      () => {
+        expect(shallowApp.dive()).toMatchSnapshot()
+        expect(spyReceiveProps.calledOnce).toBe(false);
+        shallowApp.setProps(Object.assign({}, props, {schema: store.getState().schema}))
+        expect(shallowApp.dive()).toMatchSnapshot()
+        expect(spyReceiveProps.calledOnce).toBe(true);
+      }
     )
   })
 })
