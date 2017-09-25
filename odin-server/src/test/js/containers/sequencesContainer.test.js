cc3878b2c05bf578ed30e79bdd240290205560bb
Ian Homer <ian.homer@gmail.com>
2017-09-25 16:41:29 +0100

    Assert sequences container with mount (not shallow) and ensure that assertion is not executed multiple times

--- odin-server/src/test/js/containers/sequencesContainer.test.js
+++ odin-server/src/test/js/containers/sequencesContainer.test.js
@@ -2,7 +2,7 @@ import React from 'react'
 import SequencesContainer from 'odin/containers/sequencesContainer.js'
 import { LOAD_PROJECT_SCHEMA_SUCCEEDED, LOAD_PROFILE_SCHEMA_SUCCEEDED,
   loadSchemaActions } from 'odin/actions/index.js'
-import { shallow } from 'enzyme'
+import { mount } from 'enzyme'
 
 import { dispatchAndExpect } from '../utils/dispatchAndExpect'
 import { mockFlux, testProject, testSchema } from '../testData.js'
@@ -11,13 +11,13 @@ import store from '../store'
 test('Sequences container renders OK', done => {
   const component =
     <SequencesContainer store={store} schema={testSchema} project={testProject} flux={mockFlux}/>
-  expect(shallow(component).dive()).toMatchSnapshot()
+  expect(mount(component)).toMatchSnapshot()
 
   dispatchAndExpect(store, done,
     [LOAD_PROJECT_SCHEMA_SUCCEEDED, LOAD_PROFILE_SCHEMA_SUCCEEDED, LOAD_PROFILE_SCHEMA_SUCCEEDED],
     () => loadSchemaActions,
     () => {
-      expect(shallow(component).dive()).toMatchSnapshot()
+      expect(mount(component)).toMatchSnapshot()
     }
   )
 })
--- odin-server/src/test/js/utils/dispatchAndExpect.js
+++ odin-server/src/test/js/utils/dispatchAndExpect.js
@@ -1,15 +1,18 @@
 export function dispatchAndExpect(store, done, waitForActionType, action, assertion) {
-  var unsubscriber = {}
+  var asserter = { executed : false }
   var waitForActionTypes = waitForActionType instanceof Array
     ? [ ...waitForActionType ] : [ waitForActionType ]
-  unsubscriber.unsubscribe = store.subscribe(() => {
+  asserter.unsubscribe = store.subscribe(() => {
     // The test redux store has a reducer that stores the last action so that we
     // can wait for the desired action type before executing the assertion
     waitForActionTypes = waitForActionTypes.filter(item => item !== store.getState().lastAction.type)
-    if (waitForActionTypes.length == 0) {
+    if (waitForActionTypes.length == 0 && !asserter.executed) {
+      // Set flag indicating that the assertion has been executed so that we don't apply
+      // assertion multiple times.
+      asserter.executed = true
       try {
         assertion()
-        unsubscriber.unsubscribe()
+        asserter.unsubscribe()
         done()
       } catch(e) {
         done.fail(e)
