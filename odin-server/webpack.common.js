0a26a490982d02be4115ee6d40ca92f2decabb00
Ian Homer <ian.homer@gmail.com>
2017-10-04 21:25:14 +0100

    Webpack for production by default (extra file that wouldn't commit at first)

--- odin-server/webpack.common.js
+++ odin-server/webpack.common.js
@@ -1,27 +1,25 @@
 var path = require('path');
 
 module.exports = {
-    entry: './src/main/js/index.js',
-    devtool: 'sourcemaps',
-    cache: true,
-    debug: true,
-    output: {
-        path: __dirname,
-        filename: './src/main/resources/static/built/bundle.js'
-    },
-    module: {
-        loaders: [
-            {
-                test: path.join(__dirname, '.'),
-                exclude: /(node_modules)/,
-                loader: 'babel',
-                query: {
-                    cacheDirectory: true,
-                    presets: ['es2015', 'react']
-                }
-            },
-            { test: /\.json$/, loader: "json-loader" },
-            { test: /\.coffee/, loader: "coffee-loader" }
-        ]
-    }
+  entry: './src/main/js/index.js',
+  cache: true,
+  output: {
+    path: __dirname,
+    filename: './src/main/resources/static/built/bundle.js'
+  },
+  module: {
+    loaders: [
+      {
+        test: path.join(__dirname, '.'),
+        exclude: /(node_modules)/,
+        loader: 'babel',
+        query: {
+          cacheDirectory: true,
+          presets: ['es2015', 'react']
+        }
+      },
+      { test: /\.json$/, loader: "json-loader" },
+      { test: /\.coffee/, loader: "coffee-loader" }
+    ]
+  }
 };
