2f8413fdfcdc6715eb120070773dfc829e719752
Ian Homer <ian.homer@gmail.com>
2018-01-24 12:43:39 +0000

    Test coverage for create new sequence

--- odin-server/src/test/js/containers/sequencesContainerUpdating.test.js
+++ odin-server/src/test/js/containers/sequencesContainerUpdating.test.js
@@ -11,7 +11,7 @@ import {testPerformance, testSchema} from '../testData.js'
 import store from '../store'
 
 describe('Sequence container validation', () => {
-  test('Sequences update', done => {
+  test('Sequence update', done => {
     const component =
       <SequencesContainer store={store} schema={testSchema} performance={testPerformance}/>
 
@@ -46,5 +46,41 @@ describe('Sequence container validation', () => {
     expect(changeNameRow.find('.property-channel').text()).toBe('5')
   }, TIMEOUT)
 
-  // TODO : Sequence create
+  test('Sequence create', done => {
+    const component =
+      <SequencesContainer store={store} schema={testSchema} performance={testPerformance}/>
+
+    dispatchAndExpect(store, done,
+      [LOAD_PERFORMANCE_SCHEMA_SUCCEEDED, LOAD_PROFILE_SCHEMA_SUCCEEDED, LOAD_PROFILE_SCHEMA_SUCCEEDED],
+      () => loadSchemaActions,
+      () => {
+      }
+    )
+
+    const mounted = mount(component)
+
+    debugger
+    const row = mounted.findWhere(n => n.key() == 'create-notation')
+    expect(row).toMatchSnapshot()
+
+    // Fill in the new sequence form
+    row.find('input.property-name').simulate('change', {target: {value: 'new-test-sequence', name: 'name'}})
+    row.find('input.property-notation').simulate('change', {target: {value: 'A B C', name: 'notation'}})
+    row.find('input.property-channel').simulate('change', {target: {value: '4', name: 'channel'}})
+    row.find('button.btn-primary').simulate('click')
+
+    // Verify that the new sequence is now in the redux store
+    var entities = component.props.store.getState().rest.sequence.entities
+    var newSequence = entities.find(entity => entity.name == 'new-test-sequence')
+    expect(newSequence).toBeDefined()
+
+    // Verify that the new sequence rendered on the page
+    const newNameRow = mounted.findWhere(n => n.key() == 'new-test-sequence').at(1)
+    expect(newNameRow).toMatchSnapshot()
+    expect(newNameRow.find('.property-name').text()).toBe('new-test-sequence')
+    expect(newNameRow.find('.property-notation').text()).toBe('A B C')
+    expect(newNameRow.find('.property-channel').text()).toBe('4')
+
+  }, TIMEOUT)
+
 })
