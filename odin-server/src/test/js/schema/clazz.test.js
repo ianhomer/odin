40d794750a875faf2a9798ec0343bd251050021f
Ian Homer <ian.homer@gmail.com>
2018-01-24 11:39:20 +0000

    Explicitly set hibernate entity manager since spring-boot:run is picking up old one after a maven upgrade to 3.5

--- odin-server/src/test/js/schema/clazz.test.js
+++ odin-server/src/test/js/schema/clazz.test.js
@@ -9,8 +9,12 @@ describe('Clazz validation', () => {
       'performance': testSchemaJson
     })
     var notationClazz = schema.getClazz('flow-notation')
-    var refs = {'name': {'value': 'test-name'}}
+    var refs = {
+      'name': {'value': 'test-name'},
+      'performance': {'value': 'http://localhost:8080/api/performance/1'}
+    }
     var entity = notationClazz.newInstance(refs)
     expect(entity.name).toBe('test-name')
+    //expect(entity.performance).toBe('http://localhost:8080/api/performance/1')
   })
 })
