aa1b33ec440e6df851451ef577531b728c7ece7d
Ian Homer <ian.homer@gmail.com>
2018-01-24 16:02:14 +0000

    Add performance REST profile snapshot

---
+++ odin-server/src/test/java/com/purplepip/odin/server/rest/PerformanceRestTest.java
@@ -0,0 +1,28 @@
+package com.purplepip.odin.server.rest;
+
+import lombok.extern.slf4j.Slf4j;
+import org.junit.Test;
+import org.junit.runner.RunWith;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.test.autoconfigure.web.servlet.AutoConfigureMockMvc;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.test.context.ActiveProfiles;
+import org.springframework.test.context.ContextConfiguration;
+import org.springframework.test.context.junit4.SpringRunner;
+import org.springframework.test.web.servlet.MockMvc;
+
+@RunWith(SpringRunner.class)
+@ContextConfiguration
+@SpringBootTest
+@AutoConfigureMockMvc
+@ActiveProfiles({"test", "noAuditing"})
+@Slf4j
+public class PerformanceRestTest {
+  @Autowired
+  private MockMvc mvc;
+
+  @Test
+  public void testProfile() throws Exception {
+    new EndPointSnapshot(mvc, "/api/profile/performance").expectMatch();
+  }
+}
