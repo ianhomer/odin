fc5cdd6db4b22add0a4670da6b14d5cf02a53a39
Ian Homer <ian.homer@gmail.com>
2018-01-26 11:07:07 +0000

    Enable preloaded REST outputs for sequences and layers now that snapshot supports masking out of variable IDs

--- odin-server/src/test/java/com/purplepip/odin/server/preloaded/PreLoadedSequenceRestTest.java
+++ odin-server/src/test/java/com/purplepip/odin/server/preloaded/PreLoadedSequenceRestTest.java
@@ -2,7 +2,6 @@ package com.purplepip.odin.server.preloaded;
 
 import com.purplepip.odin.server.rest.EndPointSnapshot;
 import lombok.extern.slf4j.Slf4j;
-import org.junit.Ignore;
 import org.junit.Test;
 import org.junit.runner.RunWith;
 import org.springframework.beans.factory.annotation.Autowired;
@@ -24,7 +23,6 @@ public class PreLoadedSequenceRestTest {
   private MockMvc mvc;
 
   @Test
-  @Ignore
   public void testSequencesList() throws Exception {
     new EndPointSnapshot(mvc, "/api/sequence").expectMatch();
   }
