9ae0675423dafbe1dd4e547cd1fc962b63af0f64
Ian Homer <ian.homer@gmail.com>
2018-01-28 22:16:32 +0000

    Upgrade webpack, bable-jest, babel-loader, coffee-loader, coffeescript npm modules to latest

--- odin-server/webpack.config.js
+++ odin-server/webpack.config.js
@@ -4,6 +4,5 @@ const common = require('./webpack.common.js');
 // This configuration file creates a development built.  It is used when the front end
 // is built with mvn frontend:webpack
 module.exports = merge(common, {
-  devtool: 'sourcemaps',
-  debug: true
+  devtool: 'sourcemaps'
 });
