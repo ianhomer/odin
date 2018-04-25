b498734d88562c81e2d283be7192a641e9eee0a5
Ian Homer <ian.homer@gmail.com>
2018-04-25 22:16:32 +0100

    Remove unused parameter in javadoc

--- odin-core/src/main/java/com/purplepip/odin/creation/flow/FlowFactory.java
+++ odin-core/src/main/java/com/purplepip/odin/creation/flow/FlowFactory.java
@@ -25,7 +25,7 @@ import lombok.extern.slf4j.Slf4j;
 /**
  * Factory to create flows.
  *
- * @param <A> type of object delivered by the flow.
+ * @param type of object delivered by the flow.
  */
 @Slf4j
 public class FlowFactory {
