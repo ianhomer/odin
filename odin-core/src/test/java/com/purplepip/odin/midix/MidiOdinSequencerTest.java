e6425e8e555c7167b48fcd23c0ae3abe03685123
Ian Homer <ian.homer@gmail.com>
2018-07-08 00:32:38 +0100

    Remove another use of MidiDeviceWrapper

--- odin-core/src/test/java/com/purplepip/odin/midix/MidiOdinSequencerTest.java
+++ odin-core/src/test/java/com/purplepip/odin/midix/MidiOdinSequencerTest.java
@@ -1,12 +1,14 @@
 package com.purplepip.odin.midix;
 
-import static com.purplepip.odin.system.Environments.newAudioEnvironment;
+import static com.purplepip.odin.system.Environments.isAudioEnabled;
+import static com.purplepip.odin.system.Environments.newEnvironment;
 import static org.junit.Assert.assertEquals;
 import static org.junit.Assume.assumeTrue;
 
 import com.purplepip.odin.clock.beats.StaticBeatsPerMinute;
 import com.purplepip.odin.clock.measure.MeasureProvider;
 import com.purplepip.odin.clock.measure.StaticBeatMeasureProvider;
+import com.purplepip.odin.devices.DeviceUnavailableException;
 import com.purplepip.odin.operation.OperationHandler;
 import com.purplepip.odin.performance.DefaultPerformanceContainer;
 import com.purplepip.odin.performance.TransientPerformance;
@@ -19,41 +21,43 @@ import java.util.concurrent.TimeUnit;
 import lombok.extern.slf4j.Slf4j;
 import org.junit.Test;
 
-/**
- * Odin sequencer test with real sends to MIDI.
- */
+/** Odin sequencer test with real sends to MIDI. */
 @Slf4j
 public class MidiOdinSequencerTest {
   @Test
-  public void testSequencer() throws InterruptedException {
-    assumeTrue(!newAudioEnvironment().isEmpty());
-    try (MidiDeviceWrapper midiDeviceWrapper = new MidiDeviceWrapper()) {
+  public void testSequencer() throws InterruptedException, DeviceUnavailableException {
+    assumeTrue(isAudioEnabled());
+    try (MidiDevice sink = newEnvironment().findOneSinkOrThrow(MidiHandle.class)) {
       final CountDownLatch lock = new CountDownLatch(16);
 
-      OperationHandler operationReceiver = (operation, time) -> {
-        LOG.debug("Operation countdown {}", lock.getCount());
-        lock.countDown();
-      };
+      OperationHandler operationReceiver =
+          (operation, time) -> {
+            LOG.debug("Operation countdown {}", lock.getCount());
+            lock.countDown();
+          };
 
       MeasureProvider measureProvider = new StaticBeatMeasureProvider(4);
-      OdinSequencer sequencer = new OdinSequencer(
-          new DefaultOdinSequencerConfiguration()
-              .setClockStartOffset(10000)
-              .setBeatsPerMinute(new StaticBeatsPerMinute(12000))
-              .setMeasureProvider(measureProvider)
-              .setOperationReceiver(
-                  new OperationReceiverCollection(
-                      operationReceiver,
-                      new MidiOperationReceiver(midiDeviceWrapper.getReceivingDevice())
-                  )
-              ).setMicrosecondPositionProvider(midiDeviceWrapper.getReceivingDevice()));
+      OdinSequencer sequencer =
+          new OdinSequencer(
+              new DefaultOdinSequencerConfiguration()
+                  .setClockStartOffset(10000)
+                  .setBeatsPerMinute(new StaticBeatsPerMinute(12000))
+                  .setMeasureProvider(measureProvider)
+                  .setOperationReceiver(
+                      new OperationReceiverCollection(
+                          operationReceiver,
+                          new MidiOperationReceiver(sink)))
+                  .setMicrosecondPositionProvider(sink));
 
       DefaultPerformanceContainer container =
           new DefaultPerformanceContainer(new TransientPerformance());
       container.addApplyListener(sequencer);
       new PerformanceBuilder(container)
-          .addLayer("groove").withLayers("groove")
-          .withOffset(16).withLength(16).addMetronome();
+          .addLayer("groove")
+          .withLayers("groove")
+          .withOffset(16)
+          .withLength(16)
+          .addMetronome();
       container.apply();
       sequencer.prepare();
       sequencer.start();
