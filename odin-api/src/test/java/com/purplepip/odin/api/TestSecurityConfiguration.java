ff092278b1994a4ac4db29f050cbc977cf9c4bd9
Ian Homer <ian.homer@gmail.com>
2018-07-13 10:17:06 +0100

    Set up no security for unit tests

--- odin-api/src/test/java/com/purplepip/odin/api/TestSecurityConfiguration.java
+++ odin-api/src/test/java/com/purplepip/odin/api/TestSecurityConfiguration.java
@@ -15,35 +15,21 @@
 
 package com.purplepip.odin.api;
 
-import java.util.Arrays;
-import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Profile;
 import org.springframework.security.config.annotation.web.builders.HttpSecurity;
 import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
 import org.springframework.security.config.annotation.web.configuration.WebSecurityConfigurerAdapter;
-import org.springframework.web.cors.CorsConfiguration;
-import org.springframework.web.cors.CorsConfigurationSource;
-import org.springframework.web.cors.UrlBasedCorsConfigurationSource;
 
+/**
+ * Permit all requests in test mode.
+ */
 @EnableWebSecurity
-@Profile("!test")
-public class WebSecurityConfig extends WebSecurityConfigurerAdapter {
+@Profile("test")
+public class TestSecurityConfiguration extends WebSecurityConfigurerAdapter {
   @Override
   protected void configure(HttpSecurity http) throws Exception {
-    http.cors();
-  }
-
-  @Bean
-  CorsConfigurationSource corsConfigurationSource() {
-    CorsConfiguration configuration = new CorsConfiguration();
-    configuration.setAllowedOrigins(Arrays.asList("http://localhost:63342"));
-    configuration.setAllowedMethods(
-        Arrays.asList("DELETE", "GET", "OPTIONS", "PATCH", "POST", "PUT"));
-    configuration.setAllowedHeaders(
-        Arrays.asList("Authorization", "Cache-Control", "Content-Type"));
-    configuration.setAllowCredentials(true);
-    UrlBasedCorsConfigurationSource source = new UrlBasedCorsConfigurationSource();
-    source.registerCorsConfiguration("/**", configuration);
-    return source;
+    http
+        .csrf().disable()
+        .authorizeRequests().anyRequest().permitAll();
   }
 }
