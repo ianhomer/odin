ed39004f3375fbd4ddc05eeb4e56798a213fa38b
Ian Homer <ian.homer@gmail.com>
2018-10-01 11:23:10 +0100

    Pass in number argument not string

--- odin-frontend/src/main/js/components/Tick.test.js
+++ odin-frontend/src/main/js/components/Tick.test.js
@@ -4,7 +4,7 @@ import renderer from 'react-test-renderer'
 
 test('Tick renders OK', () => {
   const component = renderer.create(
-    <Tick numerator="1" denominator="2"/>
+    <Tick numerator={1} denominator={2}/>
   )
   let tree = component.toJSON()
   expect(tree).toMatchSnapshot()
