5e8fe7a1d3b993b6da8fd743b345c69cae1f5533
Ian Homer <ian.homer@gmail.com>
2018-10-08 10:15:01 +0100

    Kick off flaky module ; each module now explicitly uses junit vintage to allow gradual removal

---
+++ common/flaky/src/main/java/com/purplepip/flaky/FlakyTestExtension.java
@@ -0,0 +1,32 @@
+/*
+ * Copyright (c) 2017 the original author or authors. All Rights Reserved
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *  http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+
+package com.purplepip.flaky;
+
+import org.junit.jupiter.api.extension.ExtensionContext;
+import org.junit.jupiter.api.extension.TestExecutionExceptionHandler;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+
+public class FlakyTestExtension implements TestExecutionExceptionHandler {
+  private static final Logger LOG = LoggerFactory.getLogger(FlakyTestExtension.class);
+
+  @Override
+  public void handleTestExecutionException(ExtensionContext extensionContext, Throwable throwable)
+      throws Throwable {
+    LOG.error("Handling exception {}", throwable);
+  }
+
+}
