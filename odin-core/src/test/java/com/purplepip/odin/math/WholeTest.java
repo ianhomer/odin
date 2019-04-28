a6594a252f618ff9d02a7562fe278ec791182676
Ian Homer <ian.homer@gmail.com>
2019-04-28 10:54:07 +0100

    Change WholeTest parametrized test to Junit 5

--- odin-core/src/test/java/com/purplepip/odin/math/WholeTest.java
+++ odin-core/src/test/java/com/purplepip/odin/math/WholeTest.java
@@ -1,20 +1,3 @@
-package com.purplepip.odin.math;
-
-import static org.junit.Assert.assertEquals;
-import static org.junit.Assert.assertFalse;
-import static org.junit.Assert.assertTrue;
-
-import java.util.ArrayList;
-import java.util.Collection;
-import java.util.function.Function;
-import lombok.Getter;
-import lombok.Setter;
-import lombok.experimental.Accessors;
-import lombok.extern.slf4j.Slf4j;
-import org.junit.Test;
-import org.junit.runner.RunWith;
-import org.junit.runners.Parameterized;
-
 /*
  * Copyright (c) 2017 the original author or authors. All Rights Reserved
  * Licensed under the Apache License, Version 2.0 (the "License");
@@ -29,123 +12,131 @@ import org.junit.runners.Parameterized;
  * See the License for the specific language governing permissions and
  * limitations under the License.
  */
-@Slf4j
-@RunWith(Parameterized.class)
-public class WholeTest {
-  private Function<Integer, Whole> newWhole;
 
+package com.purplepip.odin.math;
+
+import static org.junit.Assert.assertEquals;
+import static org.junit.Assert.assertFalse;
+import static org.junit.Assert.assertTrue;
+
+import java.util.function.Function;
+import java.util.stream.Stream;
+import lombok.extern.slf4j.Slf4j;
+import org.junit.jupiter.params.ParameterizedTest;
+import org.junit.jupiter.params.provider.MethodSource;
+
+@Slf4j
+class WholeTest {
   /**
    * Parameters for test.
    *
    * @return parameters
    */
-  @Parameterized.Parameters
-  public static Iterable<WholeTestParameter> parameters() {
-    Collection<WholeTestParameter> parameters = new ArrayList<>();
-    parameters.add(newParameter(value -> Wholes.valueOf(value.longValue())));
-    parameters.add(newParameter(value -> Wholes.mutableOf(value.longValue())));
-    return parameters;
+  static Stream<Function<Integer, Whole>> parameters() {
+    return Stream.of(
+        (value) -> Wholes.valueOf(value.longValue()),
+        (value) -> Wholes.mutableOf(value.longValue())
+    );
   }
 
-  public WholeTest(WholeTestParameter parameter) {
-    newWhole = parameter.factory;
-  }
-
-  @Test
-  public void testValueOf() {
+  @ParameterizedTest
+  @MethodSource("parameters")
+  void testValueOf(Function<Integer, Whole> newWhole) {
     assertEquals(Wholes.MINUS_ONE, newWhole.apply(-1).asImmutable());
     assertEquals(Wholes.ZERO, newWhole.apply(0).asImmutable());
   }
 
-  @Test
-  public void testPlus() {
+  @ParameterizedTest
+  @MethodSource("parameters")
+  void testPlus(Function<Integer, Whole> newWhole) {
     assertEquals(Rationals.valueOf(5,2),
         newWhole.apply(2).plus(Rationals.HALF).asImmutable());
     assertEquals(Wholes.THREE, newWhole.apply(2).plus(Wholes.ONE).asImmutable());
     assertEquals(Reals.valueOf(5.3), newWhole.apply(1).plus(Reals.valueOf(4.3)).asImmutable());
   }
 
-  @Test
-  public void testMinus() {
+  @ParameterizedTest
+  @MethodSource("parameters")
+  void testMinus(Function<Integer, Whole> newWhole) {
     assertEquals(newWhole.apply(1), newWhole.apply(2).minus(newWhole.apply(1)));
     assertEquals(Reals.valueOf(0.9).getValue(),
         newWhole.apply(2).minus(Reals.valueOf(1.1)).getValue(), Reals.DOUBLE_PRECISION);
   }
 
-  @Test
-  public void testFloor() {
+  @ParameterizedTest
+  @MethodSource("parameters")
+  void testFloor(Function<Integer, Whole> newWhole) {
     assertEquals(3, newWhole.apply(3).floor());
     assertEquals(2, Reals.valueOf(2.1).floor());
   }
 
-  @Test
-  public void testNextFloor() {
+  @ParameterizedTest
+  @MethodSource("parameters")
+  void testNextFloor(Function<Integer, Whole> newWhole) {
     assertEquals(4, newWhole.apply(3).nextFloor());
     assertEquals(3, Reals.valueOf(2.1).nextFloor());
   }
 
-  @Test
-  public void testModulo() {
+  @ParameterizedTest
+  @MethodSource("parameters")
+  void testModulo(Function<Integer, Whole> newWhole) {
     assertEquals(Wholes.valueOf(2), newWhole.apply(10).modulo(Wholes.valueOf(4)).asImmutable());
     assertEquals(Wholes.valueOf(0), newWhole.apply(10).modulo(Wholes.valueOf(1)).asImmutable());
   }
 
-  @Test
-  public void testFloorWithRadix() {
+  @ParameterizedTest
+  @MethodSource("parameters")
+  void testFloorWithRadix(Function<Integer, Whole> newWhole) {
     assertEquals(Wholes.valueOf(8), newWhole.apply(10).floor(Wholes.valueOf(4)).asImmutable());
   }
 
-  @Test
-  public void testEquals() {
+  @ParameterizedTest
+  @MethodSource("parameters")
+  public void testEquals(Function<Integer, Whole> newWhole) {
     assertEquals(Wholes.ONE, newWhole.apply(1).asImmutable());
   }
 
-  @Test
-  public void testToString() {
+  @ParameterizedTest
+  @MethodSource("parameters")
+  void testToString(Function<Integer, Whole> newWhole) {
     assertEquals("-1", newWhole.apply(-1).toString());
   }
 
-  @Test
-  public void testIsNegative() {
+  @ParameterizedTest
+  @MethodSource("parameters")
+  void testIsNegative(Function<Integer, Whole> newWhole) {
     assertFalse(newWhole.apply(1).isNegative());
     assertFalse(newWhole.apply(0).isNegative());
     assertTrue(newWhole.apply(-1).isNegative());
   }
 
-  @Test
-  public void testIsZero() {
+  @ParameterizedTest
+  @MethodSource("parameters")
+  void testIsZero(Function<Integer, Whole> newWhole) {
     assertFalse(newWhole.apply(1).isZero());
     assertTrue(newWhole.apply(0).isZero());
   }
 
-  @Test
-  public void testNegative() {
+  @ParameterizedTest
+  @MethodSource("parameters")
+  void testNegative(Function<Integer, Whole> newWhole) {
     assertEquals(Wholes.valueOf(1), newWhole.apply(-1).negative().asImmutable());
     assertEquals(Wholes.valueOf(-1), newWhole.apply(1).negative().asImmutable());
     assertEquals(Wholes.ZERO, newWhole.apply(0).asImmutable());
   }
 
-  @Test
-  public void testAbsolute() {
+  @ParameterizedTest
+  @MethodSource("parameters")
+  public void testAbsolute(Function<Integer, Whole> newWhole) {
     assertEquals(Wholes.valueOf(1), newWhole.apply(1).absolute().asImmutable());
     assertEquals(Wholes.valueOf(1), newWhole.apply(-1).absolute().asImmutable());
     assertEquals(Wholes.valueOf(0), newWhole.apply(0).absolute().asImmutable());
   }
 
-  @Test
-  public void testCeiling() {
+  @ParameterizedTest
+  @MethodSource("parameters")
+  void testCeiling(Function<Integer, Whole> newWhole) {
     assertEquals(10, newWhole.apply(10).ceiling());
     assertEquals(-13, newWhole.apply(-13).ceiling());
-  }
-
-  private static WholeTestParameter newParameter(Function<Integer, Whole> factory) {
-    return new WholeTestParameter().factory(factory);
-  }
-
-  @Accessors(fluent = true)
-  private static class WholeTestParameter {
-    @Getter
-    @Setter
-    private Function<Integer, Whole> factory;
   }
 }
