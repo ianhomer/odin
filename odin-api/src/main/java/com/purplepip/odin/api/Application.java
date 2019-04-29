64d81987d07824f529582be53f170f450ed31f9c
Ian Homer <ian.homer@gmail.com>
2019-04-29 22:15:39 +0100

    Disable more auto configuration

--- odin-api/src/main/java/com/purplepip/odin/api/Application.java
+++ odin-api/src/main/java/com/purplepip/odin/api/Application.java
@@ -19,9 +19,23 @@ import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 import org.springframework.boot.CommandLineRunner;
 import org.springframework.boot.SpringApplication;
+import org.springframework.boot.actuate.autoconfigure.endpoint.jmx.JmxEndpointAutoConfiguration;
+import org.springframework.boot.actuate.autoconfigure.health.HealthIndicatorAutoConfiguration;
+import org.springframework.boot.actuate.autoconfigure.metrics.MetricsEndpointAutoConfiguration;
+import org.springframework.boot.actuate.autoconfigure.metrics.orm.jpa.HibernateMetricsAutoConfiguration;
+import org.springframework.boot.actuate.autoconfigure.metrics.web.client.HttpClientMetricsAutoConfiguration;
+import org.springframework.boot.actuate.autoconfigure.metrics.web.tomcat.TomcatMetricsAutoConfiguration;
 import org.springframework.boot.actuate.autoconfigure.system.DiskSpaceHealthIndicatorAutoConfiguration;
+import org.springframework.boot.actuate.autoconfigure.trace.http.HttpTraceAutoConfiguration;
+import org.springframework.boot.actuate.autoconfigure.trace.http.HttpTraceEndpointAutoConfiguration;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.boot.autoconfigure.admin.SpringApplicationAdminJmxAutoConfiguration;
 import org.springframework.boot.autoconfigure.h2.H2ConsoleAutoConfiguration;
+import org.springframework.boot.autoconfigure.jmx.JmxAutoConfiguration;
+import org.springframework.boot.autoconfigure.security.servlet.SecurityAutoConfiguration;
+import org.springframework.boot.autoconfigure.security.servlet.SecurityFilterAutoConfiguration;
+import org.springframework.boot.autoconfigure.security.servlet.SecurityRequestMatcherProviderAutoConfiguration;
+import org.springframework.boot.autoconfigure.security.servlet.UserDetailsServiceAutoConfiguration;
 import org.springframework.context.ApplicationContext;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.ComponentScan;
@@ -33,7 +47,20 @@ import org.springframework.core.annotation.Order;
 @SpringBootApplication(
     exclude = {
         DiskSpaceHealthIndicatorAutoConfiguration.class,
-        H2ConsoleAutoConfiguration.class
+        H2ConsoleAutoConfiguration.class,
+        HibernateMetricsAutoConfiguration.class,
+        HttpClientMetricsAutoConfiguration.class,
+        HttpTraceAutoConfiguration.class,
+        HttpTraceEndpointAutoConfiguration.class,
+        JmxAutoConfiguration.class,
+        JmxEndpointAutoConfiguration.class,
+        MetricsEndpointAutoConfiguration.class,
+        SecurityAutoConfiguration.class,
+        SecurityFilterAutoConfiguration.class,
+        SecurityRequestMatcherProviderAutoConfiguration.class,
+        SpringApplicationAdminJmxAutoConfiguration.class,
+        TomcatMetricsAutoConfiguration.class,
+        UserDetailsServiceAutoConfiguration.class
     }
 )
 @ComponentScan("com.purplepip.odin.api")
